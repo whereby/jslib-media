da2355168da27d6a94f2b703db1972da073993c6
Pontus Fagerström <pontus.fagerstrom@whereby.com>
2023-12-19 10:29:33 +0100

    dont use sut in tests

--- tests/webrtc/VegaMediaQualityMonitor.spec.js
+++ tests/webrtc/VegaMediaQualityMonitor.spec.js
@@ -15,39 +15,39 @@ const logger = {
 
 describe("VegaMediaQualityMonitor", () => {
     it("should keep track of remote clients producer score", () => {
-        const sut = new VegaMediaQualityMonitor({ logger });
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
 
-        sut.addConsumer(CLIENT_ID1, CONSUMER_ID1);
-        sut.addConsumer(CLIENT_ID2, CONSUMER_ID2);
-        sut.addConsumer(CLIENT_ID2, CONSUMER_ID3);
-        sut.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", { producerScores: [10, 10, 10] });
-        sut.addConsumerScore(CLIENT_ID2, CONSUMER_ID2, "audio", { producerScores: [5] });
-        sut.addConsumerScore(CLIENT_ID2, CONSUMER_ID3, "video", { producerScores: [8, 0] });
+        vegaQualityMonitor.addConsumer(CLIENT_ID1, CONSUMER_ID1);
+        vegaQualityMonitor.addConsumer(CLIENT_ID2, CONSUMER_ID2);
+        vegaQualityMonitor.addConsumer(CLIENT_ID2, CONSUMER_ID3);
+        vegaQualityMonitor.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", { producerScores: [10, 10, 10] });
+        vegaQualityMonitor.addConsumerScore(CLIENT_ID2, CONSUMER_ID2, "audio", { producerScores: [5] });
+        vegaQualityMonitor.addConsumerScore(CLIENT_ID2, CONSUMER_ID3, "video", { producerScores: [8, 0] });
 
-        expect(Object.keys(sut._producers).length).toBe(2);
-        expect(sut._producers[CLIENT_ID1][CONSUMER_ID1].kind).toBe("video");
-        expect(sut._producers[CLIENT_ID1][CONSUMER_ID1].score).toBe(10);
-        expect(sut._producers[CLIENT_ID2][CONSUMER_ID2].kind).toBe("audio");
-        expect(sut._producers[CLIENT_ID2][CONSUMER_ID2].score).toBe(5);
-        expect(sut._producers[CLIENT_ID2][CONSUMER_ID3].kind).toBe("video");
-        expect(sut._producers[CLIENT_ID2][CONSUMER_ID3].score).toBe(8);
-        sut.close();
+        expect(Object.keys(vegaQualityMonitor._producers).length).toBe(2);
+        expect(vegaQualityMonitor._producers[CLIENT_ID1][CONSUMER_ID1].kind).toBe("video");
+        expect(vegaQualityMonitor._producers[CLIENT_ID1][CONSUMER_ID1].score).toBe(10);
+        expect(vegaQualityMonitor._producers[CLIENT_ID2][CONSUMER_ID2].kind).toBe("audio");
+        expect(vegaQualityMonitor._producers[CLIENT_ID2][CONSUMER_ID2].score).toBe(5);
+        expect(vegaQualityMonitor._producers[CLIENT_ID2][CONSUMER_ID3].kind).toBe("video");
+        expect(vegaQualityMonitor._producers[CLIENT_ID2][CONSUMER_ID3].score).toBe(8);
+        vegaQualityMonitor.close();
     });
 
     it("should keep track of local producer score", () => {
-        const sut = new VegaMediaQualityMonitor({ logger });
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
 
-        sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
-        sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID2);
-        sut.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID1, "video", [{ score: 10 }, { score: 0 }]);
-        sut.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID2, "audio", [{ score: 8 }]);
+        vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
+        vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID2);
+        vegaQualityMonitor.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID1, "video", [{ score: 10 }, { score: 0 }]);
+        vegaQualityMonitor.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID2, "audio", [{ score: 8 }]);
 
-        expect(Object.keys(sut._producers).length).toBe(1);
-        expect(sut._producers[SELF_CLIENT_ID][PRODUCER_ID1].kind).toBe("video");
-        expect(sut._producers[SELF_CLIENT_ID][PRODUCER_ID1].score).toBe(10);
-        expect(sut._producers[SELF_CLIENT_ID][PRODUCER_ID2].kind).toBe("audio");
-        expect(sut._producers[SELF_CLIENT_ID][PRODUCER_ID2].score).toBe(8);
-        sut.close();
+        expect(Object.keys(vegaQualityMonitor._producers).length).toBe(1);
+        expect(vegaQualityMonitor._producers[SELF_CLIENT_ID][PRODUCER_ID1].kind).toBe("video");
+        expect(vegaQualityMonitor._producers[SELF_CLIENT_ID][PRODUCER_ID1].score).toBe(10);
+        expect(vegaQualityMonitor._producers[SELF_CLIENT_ID][PRODUCER_ID2].kind).toBe("audio");
+        expect(vegaQualityMonitor._producers[SELF_CLIENT_ID][PRODUCER_ID2].score).toBe(8);
+        vegaQualityMonitor.close();
     });
 
     it.each([
@@ -55,13 +55,13 @@ describe("VegaMediaQualityMonitor", () => {
         [[{ score: 10 }, { score: 0 }], 10],
         [[{ score: 0 }, { score: 0 }], 0],
     ])("should calculate score average on simulcast/svc local producer score: %o, avg: %s", (score, avg) => {
-        const sut = new VegaMediaQualityMonitor({ logger });
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
 
-        sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
-        sut.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID1, "video", score);
+        vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
+        vegaQualityMonitor.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID1, "video", score);
 
-        expect(sut._producers[SELF_CLIENT_ID][PRODUCER_ID1].score).toBe(avg);
-        sut.close();
+        expect(vegaQualityMonitor._producers[SELF_CLIENT_ID][PRODUCER_ID1].score).toBe(avg);
+        vegaQualityMonitor.close();
     });
 
     it.each([
@@ -69,86 +69,86 @@ describe("VegaMediaQualityMonitor", () => {
         [{ producerScores: [10, 0] }, 10],
         [{ producerScores: [0, 0] }, 0],
     ])("should calculate score average on simulcast/svc remote producer score: %o, avg: %s", (score, avg) => {
-        const sut = new VegaMediaQualityMonitor({ logger });
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
 
-        sut.addConsumer(CLIENT_ID1, CONSUMER_ID1);
-        sut.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", score);
+        vegaQualityMonitor.addConsumer(CLIENT_ID1, CONSUMER_ID1);
+        vegaQualityMonitor.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", score);
 
-        expect(sut._producers[CLIENT_ID1][CONSUMER_ID1].score).toBe(avg);
-        sut.close();
+        expect(vegaQualityMonitor._producers[CLIENT_ID1][CONSUMER_ID1].score).toBe(avg);
+        vegaQualityMonitor.close();
     });
 
     it("should not remove remote client with active producers", () => {
-        const sut = new VegaMediaQualityMonitor({ logger });
-        sut.addConsumer(CLIENT_ID1, CONSUMER_ID1);
-        sut.addConsumer(CLIENT_ID1, CONSUMER_ID2);
-        sut.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", { producerScores: [10, 10, 10] });
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
+        vegaQualityMonitor.addConsumer(CLIENT_ID1, CONSUMER_ID1);
+        vegaQualityMonitor.addConsumer(CLIENT_ID1, CONSUMER_ID2);
+        vegaQualityMonitor.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", { producerScores: [10, 10, 10] });
 
-        sut.removeConsumer(CLIENT_ID1, CONSUMER_ID2);
+        vegaQualityMonitor.removeConsumer(CLIENT_ID1, CONSUMER_ID2);
 
-        expect(Object.keys(sut._producers).length).toBe(1);
-        expect(sut._producers[CLIENT_ID1][CONSUMER_ID1].kind).toBe("video");
-        expect(sut._producers[CLIENT_ID1][CONSUMER_ID1].score).toBe(10);
-        sut.close();
+        expect(Object.keys(vegaQualityMonitor._producers).length).toBe(1);
+        expect(vegaQualityMonitor._producers[CLIENT_ID1][CONSUMER_ID1].kind).toBe("video");
+        expect(vegaQualityMonitor._producers[CLIENT_ID1][CONSUMER_ID1].score).toBe(10);
+        vegaQualityMonitor.close();
     });
 
     it("should remove remote client without active producers", () => {
-        const sut = new VegaMediaQualityMonitor({ logger });
-        sut.addConsumer(CLIENT_ID1, CONSUMER_ID1);
-        sut.addConsumer(CLIENT_ID1, CONSUMER_ID2);
-        sut.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", { producerScores: [10, 10, 10] });
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
+        vegaQualityMonitor.addConsumer(CLIENT_ID1, CONSUMER_ID1);
+        vegaQualityMonitor.addConsumer(CLIENT_ID1, CONSUMER_ID2);
+        vegaQualityMonitor.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", { producerScores: [10, 10, 10] });
 
-        sut.removeConsumer(CLIENT_ID1, CONSUMER_ID1);
-        sut.removeConsumer(CLIENT_ID1, CONSUMER_ID2);
+        vegaQualityMonitor.removeConsumer(CLIENT_ID1, CONSUMER_ID1);
+        vegaQualityMonitor.removeConsumer(CLIENT_ID1, CONSUMER_ID2);
 
-        expect(Object.keys(sut._producers).length).toBe(0);
-        expect(sut._producers[CLIENT_ID1]).toBeUndefined();
-        sut.close();
+        expect(Object.keys(vegaQualityMonitor._producers).length).toBe(0);
+        expect(vegaQualityMonitor._producers[CLIENT_ID1]).toBeUndefined();
+        vegaQualityMonitor.close();
     });
 
     it("should not remove self client with active producers", () => {
-        const sut = new VegaMediaQualityMonitor({ logger });
-        sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
-        sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID2);
-        sut.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID2, "video", [{ score: 10 }]);
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
+        vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
+        vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID2);
+        vegaQualityMonitor.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID2, "video", [{ score: 10 }]);
 
-        sut.removeProducer(SELF_CLIENT_ID, PRODUCER_ID1);
+        vegaQualityMonitor.removeProducer(SELF_CLIENT_ID, PRODUCER_ID1);
 
-        expect(Object.keys(sut._producers).length).toBe(1);
-        expect(sut._producers[SELF_CLIENT_ID][PRODUCER_ID2].kind).toBe("video");
-        expect(sut._producers[SELF_CLIENT_ID][PRODUCER_ID2].score).toBe(10);
-        sut.close();
+        expect(Object.keys(vegaQualityMonitor._producers).length).toBe(1);
+        expect(vegaQualityMonitor._producers[SELF_CLIENT_ID][PRODUCER_ID2].kind).toBe("video");
+        expect(vegaQualityMonitor._producers[SELF_CLIENT_ID][PRODUCER_ID2].score).toBe(10);
+        vegaQualityMonitor.close();
     });
 
     it("should remove remote client without active producers", () => {
-        const sut = new VegaMediaQualityMonitor({ logger });
-        sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
-        sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID2);
-        sut.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID2, "video", [{ score: 10 }]);
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
+        vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
+        vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID2);
+        vegaQualityMonitor.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID2, "video", [{ score: 10 }]);
 
-        sut.removeProducer(SELF_CLIENT_ID, PRODUCER_ID1);
-        sut.removeProducer(SELF_CLIENT_ID, PRODUCER_ID2);
+        vegaQualityMonitor.removeProducer(SELF_CLIENT_ID, PRODUCER_ID1);
+        vegaQualityMonitor.removeProducer(SELF_CLIENT_ID, PRODUCER_ID2);
 
-        expect(Object.keys(sut._producers).length).toBe(0);
-        expect(sut._producers[SELF_CLIENT_ID]).toBeUndefined();
-        sut.close();
+        expect(Object.keys(vegaQualityMonitor._producers).length).toBe(0);
+        expect(vegaQualityMonitor._producers[SELF_CLIENT_ID]).toBeUndefined();
+        vegaQualityMonitor.close();
     });
 
     it("should cleanup on close", async () => {
-        const sut = new VegaMediaQualityMonitor({ logger });
-        sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
-        sut.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID1, "video", [{ score: 10 }]);
-        sut.addConsumer(CLIENT_ID1, CONSUMER_ID1);
-        sut.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", { producerScores: [10, 10, 10] });
+        const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
+        vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
+        vegaQualityMonitor.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID1, "video", [{ score: 10 }]);
+        vegaQualityMonitor.addConsumer(CLIENT_ID1, CONSUMER_ID1);
+        vegaQualityMonitor.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", { producerScores: [10, 10, 10] });
 
         const delay = new Promise((resolve) => {
             setTimeout(() => {
-                expect(Object.keys(sut._producers).length).toBe(2);
-                expect(Object.keys(sut._clients).length).toBe(2);
-                sut.close();
-                expect(Object.keys(sut._producers).length).toBe(0);
-                expect(Object.keys(sut._clients).length).toBe(0);
-                expect(sut._intervalHandle).toBeUndefined();
+                expect(Object.keys(vegaQualityMonitor._producers).length).toBe(2);
+                expect(Object.keys(vegaQualityMonitor._clients).length).toBe(2);
+                vegaQualityMonitor.close();
+                expect(Object.keys(vegaQualityMonitor._producers).length).toBe(0);
+                expect(Object.keys(vegaQualityMonitor._clients).length).toBe(0);
+                expect(vegaQualityMonitor._intervalHandle).toBeUndefined();
                 resolve();
             }, 3200);
         });
@@ -158,44 +158,48 @@ describe("VegaMediaQualityMonitor", () => {
     it.each([[null], [undefined], [{}], [[]], [[{ score: 1 }, {}]], [[{ score: 10 }, null]]])(
         "should not throw on unexpected producer score format: %o",
         (illegalScore) => {
-            const sut = new VegaMediaQualityMonitor({ logger });
-            sut.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
+            const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
+            vegaQualityMonitor.addProducer(SELF_CLIENT_ID, PRODUCER_ID1);
 
-            expect(() => sut.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID1, "video", illegalScore)).not.toThrow();
+            expect(() =>
+                vegaQualityMonitor.addProducerScore(SELF_CLIENT_ID, PRODUCER_ID1, "video", illegalScore)
+            ).not.toThrow();
 
-            sut.close();
+            vegaQualityMonitor.close();
         }
     );
 
     it.each([[null], [undefined], [{}], [[]], [[{ score: 1 }, {}]], [[{ score: 10 }, null]]])(
         "should not throw on unexpected consumer score format: %o",
         (illegalScore) => {
-            const sut = new VegaMediaQualityMonitor({ logger });
-            sut.addConsumer(CLIENT_ID1, CONSUMER_ID1);
+            const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
+            vegaQualityMonitor.addConsumer(CLIENT_ID1, CONSUMER_ID1);
 
-            expect(() => sut.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", illegalScore)).not.toThrow();
+            expect(() =>
+                vegaQualityMonitor.addConsumerScore(CLIENT_ID1, CONSUMER_ID1, "video", illegalScore)
+            ).not.toThrow();
 
-            sut.close();
+            vegaQualityMonitor.close();
         }
     );
 
     it.each([[[]], [["id", null]], [[undefined, "id"]], [["id"]]])(
         "should ignore illegal consumer params %o",
         (illegalParams) => {
-            const sut = new VegaMediaQualityMonitor({ logger });
+            const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
 
-            expect(() => sut.addConsumer(illegalParams)).not.toThrow();
-            expect(Object.keys(sut._producers).length).toBe(0);
+            expect(() => vegaQualityMonitor.addConsumer(illegalParams)).not.toThrow();
+            expect(Object.keys(vegaQualityMonitor._producers).length).toBe(0);
         }
     );
 
     it.each([[[]], [["id", null]], [[undefined, "id"]], [["id"]]])(
         "should ignore illegal producer params %o",
         (illegalParams) => {
-            const sut = new VegaMediaQualityMonitor({ logger });
+            const vegaQualityMonitor = new VegaMediaQualityMonitor({ logger });
 
-            expect(() => sut.addProducer(illegalParams)).not.toThrow();
-            expect(Object.keys(sut._producers).length).toBe(0);
+            expect(() => vegaQualityMonitor.addProducer(illegalParams)).not.toThrow();
+            expect(Object.keys(vegaQualityMonitor._producers).length).toBe(0);
         }
     );
 });
