9314c77f99fc2362055c597a5668698f8cf04456
Mészáros Mihály <misi@majd.eu>
2024-01-31 21:46:31 +0100

    Add RTP header extension abs-capture-time

--- src/webrtc/sdpModifier.js
+++ src/webrtc/sdpModifier.js
@@ -171,3 +171,66 @@ export function changeMediaDirection(sdp, active) {
             .join("")
     );
 }
+
+// Add sdp RTP extension mappings
+// a=extmap:9 http://www.webrtc.org/experiments/rtp-hdrext/abs-capture-time
+export function addAbsCaptureTimeExtMap(sdp) {
+    try {
+        const sdpObj = sdpTransform.parse(sdp);
+        const absCaptureTimeUri = "http://www.webrtc.org/experiments/rtp-hdrext/abs-capture-time";
+        let extmapId;
+        let allHeaderExtensions = [];
+
+        // Collect all unique extmaps to find all already used id-s
+        sdpObj?.media.forEach((mediaSection) => {
+            if (mediaSection.ext) {
+                allHeaderExtensions = allHeaderExtensions.concat(mediaSection.ext);
+            }
+        });
+        if (allHeaderExtensions.length === 0) {
+            extmapId = 1;
+        } else {
+            allHeaderExtensions.sort((a, b) => a.value - b.value);
+            // unique
+            allHeaderExtensions = allHeaderExtensions.filter((e, idx, arr) => {
+                if (idx < allHeaderExtensions.length - 1) return e.value < arr[idx + 1].value;
+                else return true;
+            });
+            const headerExtensionGapAfter = allHeaderExtensions.find((e, idx, arr) => {
+                if (idx < allHeaderExtensions.length - 1) return arr[idx + 1].value - e.value > 1;
+                else return false;
+            });
+            if (headerExtensionGapAfter) {
+                extmapId = headerExtensionGapAfter.value + 1;
+            } else {
+                const maxid = allHeaderExtensions.pop().value;
+                if (maxid < 14) {
+                    extmapId = maxid + 1;
+                } else {
+                    // https://datatracker.ietf.org/doc/html/rfc8285#section-4.2
+                    // 15 is reserved for a future extension and MUST NOT be used
+                    if (maxid === 14) extmapId = 16;
+                    else {
+                        if (sdpObj?.extmapAllowMixed && maxid < 254) extmapId = maxid + 1;
+                        // We didn't find appropriate ID and we can't manipulate sdp
+                        else return sdp;
+                    }
+                }
+            }
+        }
+        sdpObj.media.forEach((mediaSection) => {
+            if (mediaSection.type === "audio" || mediaSection.type === "video") {
+                if (!mediaSection.ext?.find((e) => e.uri === absCaptureTimeUri)) {
+                    if (typeof mediaSection.ext === Array) {
+                        mediaSection["ext"].push({ value: extmapId, uri: absCaptureTimeUri });
+                    } else {
+                        mediaSection["ext"] = [{ value: extmapId, uri: absCaptureTimeUri }];
+                    }
+                }
+            }
+        });
+        return sdpTransform.write(sdpObj);
+    } catch (error) {
+        console.error("Error during addAbsCaptureTimeExtMap: ", error);
+    }
+}
--- tests/webrtc/sdpModifier.spec.js
+++ tests/webrtc/sdpModifier.spec.js
@@ -1,4 +1,5 @@
 import * as sdpModifier from "../../src/webrtc/sdpModifier";
+import SDPUtils from "sdp";
 
 describe("sdpModifier", () => {
     const videoSdpLines = [
@@ -62,6 +63,155 @@ describe("sdpModifier", () => {
         it("ignores m-lines without h264 payloads", () => {
             const modifedSdp = sdpModifier.deprioritizeH264(sdp);
             expect(modifedSdp).toContain("m=video 9 UDP/TLS/RTP/SAVPF 111\r\n");
+        });
+    });
+
+    describe("abs capture time", () => {
+        const sdp = (extmap) => {
+            return (
+                "v=0\r\n" +
+                "o=- 3699601008446741681 2 IN IP4 127.0.0.1\r\n" +
+                "s=-\r\n" +
+                "t=0 0\r\n" +
+                "a=group:BUNDLE 0 1\r\n" +
+                "a=extmap-allow-mixed\r\n" +
+                "m=audio 8337 UDP/TLS/RTP/SAVPF 111\r\n" +
+                "c=IN IP4 52.19.129.231\r\n" +
+                "a=rtcp:9 IN IP4 0.0.0.0\r\n" +
+                "a=fingerprint:sha-256 2F:50:3C:E5:ED:C2:2F:61:2B:7F:5D:8A:32:60:62:AC:FE:79:53:9E:90:F4:4D:71:E0:A1:11:CB:79:5C:6F:53\r\n" +
+                "a=mid:0\r\n" +
+                extmap["audio"] +
+                "a=sendrecv\r\n" +
+                "a=rtcp-mux\r\n" +
+                "a=rtpmap:111 opus/48000/2\r\n" +
+                "a=rtcp-fb:111 transport-cc\r\n" +
+                "a=fmtp:111 minptime=10;useinbandfec=1\r\n" +
+                "m=video 2121 UDP/TLS/RTP/SAVPF 45\r\n" +
+                "c=IN IP4 52.19.129.231\r\n" +
+                "a=rtcp:9 IN IP4 0.0.0.0\r\n" +
+                "a=fingerprint:sha-256 2F:50:3C:E5:ED:C2:2F:61:2B:7F:5D:8A:32:60:62:AC:FE:79:53:9E:90:F4:4D:71:E0:A1:11:CB:79:5C:6F:53\r\n" +
+                "a=mid:1\r\n" +
+                extmap["video"] +
+                "a=sendrecv\r\n" +
+                "a=rtcp-mux\r\n" +
+                "a=rtcp-rsize\r\n" +
+                "a=rtpmap:45 AV1/90000\r\n" +
+                "a=rtcp-fb:45 goog-remb\r\n" +
+                "a=rtcp-fb:45 transport-cc\r\n" +
+                "a=rtcp-fb:45 ccm fir\r\n" +
+                "a=rtcp-fb:45 nack\r\n" +
+                "a=rtcp-fb:45 nack pli\r\n"
+            );
+        };
+        it("Add RTP header extension map abs-capture-time no ext map id=1", () => {
+            const extmap = { audio: "", video: "" };
+            const modifedSdp = sdpModifier.addAbsCaptureTimeExtMap(sdp(extmap));
+            const sections = SDPUtils.splitSections(modifedSdp);
+            sections.forEach((mediaSection) => {
+                const kind = SDPUtils.getKind(mediaSection);
+                if (kind === "audio" || kind === "video") {
+                    expect(mediaSection).toContain(
+                        "a=extmap:1 http://www.webrtc.org/experiments/rtp-hdrext/abs-capture-time\r\n"
+                    );
+                }
+            });
+        });
+
+        it("Add RTP header extension map abs-capture-time id=9", () => {
+            const extmap = {
+                audio:
+                    "a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level\r\n" +
+                    "a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n" +
+                    "a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r\n" +
+                    "a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r\n",
+                video:
+                    "a=extmap:14 urn:ietf:params:rtp-hdrext:toffset\r\n" +
+                    "a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n" +
+                    "a=extmap:13 urn:3gpp:video-orientation\r\n" +
+                    "a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r\n" +
+                    "a=extmap:5 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay\r\n" +
+                    "a=extmap:6 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type\r\n" +
+                    "a=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing\r\n" +
+                    "a=extmap:8 http://www.webrtc.org/experiments/rtp-hdrext/color-space\r\n" +
+                    "a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r\n" +
+                    "a=extmap:10 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id\r\n" +
+                    "a=extmap:11 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id\r\n",
+            };
+            const modifedSdp = sdpModifier.addAbsCaptureTimeExtMap(sdp(extmap));
+            const sections = SDPUtils.splitSections(modifedSdp);
+            sections.forEach((mediaSection) => {
+                const kind = SDPUtils.getKind(mediaSection);
+                if (kind === "audio" || kind === "video") {
+                    expect(mediaSection).toContain(
+                        "a=extmap:9 http://www.webrtc.org/experiments/rtp-hdrext/abs-capture-time\r\n"
+                    );
+                }
+            });
+        });
+
+        it("Add RTP header extension map abs-capture-time id=12", () => {
+            const extmap = {
+                audio:
+                    "a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level\r\n" +
+                    "a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n" +
+                    "a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r\n" +
+                    "a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r\n",
+                video:
+                    "a=extmap:9 urn:ietf:params:rtp-hdrext:toffset\r\n" +
+                    "a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n" +
+                    "a=extmap:13 urn:3gpp:video-orientation\r\n" +
+                    "a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r\n" +
+                    "a=extmap:5 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay\r\n" +
+                    "a=extmap:6 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type\r\n" +
+                    "a=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing\r\n" +
+                    "a=extmap:8 http://www.webrtc.org/experiments/rtp-hdrext/color-space\r\n" +
+                    "a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r\n" +
+                    "a=extmap:10 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id\r\n" +
+                    "a=extmap:11 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id\r\n",
+            };
+            const modifedSdp = sdpModifier.addAbsCaptureTimeExtMap(sdp(extmap));
+            const sections = SDPUtils.splitSections(modifedSdp);
+            sections.forEach((mediaSection) => {
+                const kind = SDPUtils.getKind(mediaSection);
+                if (kind === "audio" || kind === "video") {
+                    expect(mediaSection).toContain(
+                        "a=extmap:12 http://www.webrtc.org/experiments/rtp-hdrext/abs-capture-time\r\n"
+                    );
+                }
+            });
+        });
+        it("Add RTP header extension map abs-capture-time (skip reserved 15) id=16", () => {
+            const extmap = {
+                audio:
+                    "a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level\r\n" +
+                    "a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n" +
+                    "a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r\n" +
+                    "a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r\n" +
+                    "a=extmap:12 http://www.webrtc.org/experiments/rtp-hdrext/inband-c\r\n",
+                video:
+                    "a=extmap:9 urn:ietf:params:rtp-hdrext:toffset\r\n" +
+                    "a=extmap:2 http://www.webrtc.org/experiments/rtp-hdrext/abs-send-time\r\n" +
+                    "a=extmap:13 urn:3gpp:video-orientation\r\n" +
+                    "a=extmap:3 http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01\r\n" +
+                    "a=extmap:5 http://www.webrtc.org/experiments/rtp-hdrext/playout-delay\r\n" +
+                    "a=extmap:6 http://www.webrtc.org/experiments/rtp-hdrext/video-content-type\r\n" +
+                    "a=extmap:7 http://www.webrtc.org/experiments/rtp-hdrext/video-timing\r\n" +
+                    "a=extmap:8 http://www.webrtc.org/experiments/rtp-hdrext/color-space\r\n" +
+                    "a=extmap:4 urn:ietf:params:rtp-hdrext:sdes:mid\r\n" +
+                    "a=extmap:10 urn:ietf:params:rtp-hdrext:sdes:rtp-stream-id\r\n" +
+                    "a=extmap:11 urn:ietf:params:rtp-hdrext:sdes:repaired-rtp-stream-id\r\n" +
+                    "a=extmap:14 http://www.webrtc.org/experiments/rtp-hdrext/video-layers-allocation00",
+            };
+            const modifedSdp = sdpModifier.addAbsCaptureTimeExtMap(sdp(extmap));
+            const sections = SDPUtils.splitSections(modifedSdp);
+            sections.forEach((mediaSection) => {
+                const kind = SDPUtils.getKind(mediaSection);
+                if (kind === "audio" || kind === "video") {
+                    expect(mediaSection).toContain(
+                        "a=extmap:16 http://www.webrtc.org/experiments/rtp-hdrext/abs-capture-time\r\n"
+                    );
+                }
+            });
         });
     });
 });
